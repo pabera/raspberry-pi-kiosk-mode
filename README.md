35b6b9396d1f98d4320553ed53496f67326c1a8a
pabera <1260686+pabera@users.noreply.github.com>
2024-03-06 08:22:40 +0100

    Provide basic script and readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# raspberry-pi-kiosk-mode
-1-liner install script to turn your Raspberry Pi into Kiosk Mode
+# Raspberry Pi Kiosk Mode
+
+This script facilitates the setup of a Kiosk Mode on Raspberry Pi devices with a streamlined 1-line installation process. It configures a headless Chromium Browser to automatically launch upon boot, loading a predefined URL.
+
+## Installation
+
+1. Begin with a clean installation of **RaspianOS Lite** (ensure it's not the full version).
+2. Upon the first boot, establish an SSH connection to your Raspberry Pi using a terminal.
+3. Copy the provided installation script and execute it in your terminal.
+
+```
+cd; bash <(wget -qO- https://raw.githubusercontent.com/pabera/raspberry-pi-kiosk-mode/main/install.sh)
+```
---
+++ install.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+# Check if the parameter is provided
+if [ -z "$1" ]; then
+    # If not provided, prompt the user for input
+    read -p "Please enter the URL you would like to open in Kiosk Mode: " url
+else
+    # If provided, use the provided value
+    url="$1"
+fi
+
+KIOSK_MODE_CONF_HEADER="## Kiosk Mode ##"
+KIOSK_MODE_XINITRC='/etc/xdg/openbox/autostart'
+KIOSK_MODE_BASHRC="${HOME}/.bashrc"
+KIOSK_MODE_CHROMIUM_CUSTOM_DISABLE_UPDATE_CHECK='/etc/chromium-browser/customizations/01-disable-update-check'
+KIOSK_MODE_CHROMIUM_FLAG_UPDATE_INTERVAL='--check-for-update-interval=31536000'
+
+_kiosk_mode_install_os_dependencies() {
+  echo "Install Kiosk Mode dependencies"
+  sudo apt-get -qq update && sudo apt-get -qq -y upgrade
+  sudo apt-get -qq -y install --no-install-recommends \
+    xserver-xorg \
+    x11-xserver-utils \
+    xinit \
+    openbox \
+    chromium-browser
+}
+
+_kiosk_mode_set_autostart() {
+  echo "Configure Kiosk Mode"
+  local url="$1"
+  local _DISPLAY='$DISPLAY'
+  local _XDG_VTNR='$XDG_VTNR'
+
+  tee -a "${KIOSK_MODE_BASHRC}" <<-EOF
+
+${KIOSK_MODE_CONF_HEADER}
+[[ -z $_DISPLAY && $_XDG_VTNR -eq 1 ]] && startx -- -nocursor
+EOF
+
+  sudo tee -a "${KIOSK_MODE_XINITRC}" <<-EOF
+
+${KIOSK_MODE_CONF_HEADER}
+# Disable any form of screen saver / screen blanking / power management
+xset s off
+xset s noblank
+xset -dpms
+
+# Start Chromium in kiosk mode
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+chromium-browser ${url} \
+  --disable-infobars \
+  --disable-pinch \
+  --disable-translate \
+  --kiosk \
+  --noerrdialogs \
+  --no-first-run
+EOF
+
+    # Autologin
+    sudo raspi-config nonint do_boot_behaviour B2
+    # Wait for network at boot
+    sudo raspi-config nonint do_boot_wait 1
+    # power management of wifi: switch off to avoid disconnecting
+    sudo iwconfig wlan0 power off
+}
+
+_kiosk_mode_update_settings() {
+  sudo mkdir -p $(dirname "${KIOSK_MODE_CHROMIUM_CUSTOM_DISABLE_UPDATE_CHECK}")
+  sudo rm -f "${KIOSK_MODE_CHROMIUM_CUSTOM_DISABLE_UPDATE_CHECK}"
+  sudo tee -a "${KIOSK_MODE_CHROMIUM_CUSTOM_DISABLE_UPDATE_CHECK}" <<-EOF
+${KIOSK_MODE_CONF_HEADER}
+CHROMIUM_FLAGS=\"\$\{CHROMIUM_FLAGS\} --check-for-update-interval=31536000\"
+EOF
+}
+
+
+_kiosk_mode_install_os_dependencies
+_kiosk_mode_set_autostart "$url"
+_kiosk_mode_update_settings
